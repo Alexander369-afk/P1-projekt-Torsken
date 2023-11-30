b3c07c44f4e7f7e4f03927a61a970d7c4a8940b5
Taferz <52066169+Taferz@users.noreply.github.com>
2023-11-30 17:11:11 +0100

    Callback
    The Torsk calls back 3 second later

--- Assets/Scripts/Intro/DeclineCall.cs
+++ Assets/Scripts/Intro/DeclineCall.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class DeclineCall : MonoBehaviour
@@ -7,14 +9,20 @@ public class DeclineCall : MonoBehaviour
     public GameObject[] declineButton;
       void Start()
     {
-        declineButton = GameObject.FindGameObjectsWithTag("IntroTag");
+        // declineButton = GameObject.FindGameObjectsWithTag("IntroTag");
     }
-    public void Call() 
-    {
+    public void Call() {
         foreach (GameObject button in declineButton)
         {
             button.SetActive(false);
+            
         }
-        
+        Invoke("ReactivateGameObjects",3f);
     }
+    void ReactivateGameObjects() {
+
+        foreach (GameObject button in declineButton)
+        {
+            button.SetActive(true);
+        }}
 }
