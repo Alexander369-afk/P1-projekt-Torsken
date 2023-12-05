d808c8cf3fd590b9d39a29116f14b02f333b2ffe
Garioo <marius@qvarnstrom.dk>
2023-12-05 14:59:41 +0100

    NATHALIE!!!!!!

--- Assets/Scripts/Path.cs
+++ Assets/Scripts/Path.cs
@@ -1,61 +1,45 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Path : MonoBehaviour
 {
-   [SerializeField] Transform[] Waypoints;
+    [SerializeField] Transform[] Waypoints;
+    [SerializeField] private float[] moveSpeeds; // Array of move speeds for each waypoint
+    [SerializeField] private float[] waitTimes;  // Array of wait times for each waypoint
 
-   [SerializeField] private float moveSpeed = 2;
+    private float timer;
+    private int waypointsIndex;
 
-    int nextScene;
-    public static int spil1done = 0; //bør være en true/false
-
-    public int waypointsIndex;
-    //private void OnTriggerEnter2D(Collider2D collision)
-    //{
-        //if (collision.CompareTag("Spil1"))
-           // { 
-              //  Debug.Log("Canvas touched");
-            //if (collision.CompareTag("eyo"))
-               // Debug.Log("Canvas EYO");
-           // if (collision.CompareTag("YOYO"))
-           //     Debug.Log("EYEHO");
-        //}
-// }
     // Start is called before the first frame update
     void Start()
     {
-
-        
+        waypointsIndex = 0;
+        timer = 0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        nextScene = Torsken.sceneCount;
-       
-        if (nextScene == 8) {
-            Debug.Log("Hamna");
+        if (waypointsIndex < Waypoints.Length)
+        {
+            float distance = Vector2.Distance(transform.position, Waypoints[waypointsIndex].position);
 
-            transform.position = Waypoints[waypointsIndex].transform.position;
-
-            
-
-            if (waypointsIndex <= Waypoints.Length - 1)
+            if (distance > 0.1f)
             {
+                // Move towards the waypoint with the specified move speed
+                transform.position = Vector2.MoveTowards(transform.position, Waypoints[waypointsIndex].position, moveSpeeds[waypointsIndex] * Time.deltaTime);
+            }
+            else
+            {
+                // Arrived at the waypoint, start waiting
+                timer += Time.deltaTime;
 
-                transform.position = Vector2.MoveTowards(transform.position, Waypoints[waypointsIndex].transform.position, moveSpeed * Time.deltaTime);
-
-                Debug.Log("Waypoint " + waypointsIndex);
-
-                if (transform.position == Waypoints[waypointsIndex].transform.position)
+                if (timer >= waitTimes[waypointsIndex])
                 {
-                    waypointsIndex += 1;
+                    // Reset the timer and move to the next waypoint
+                    timer = 0f;
+                    waypointsIndex++;
                 }
-            } else {
-                spil1done++;
-                nextScene++;
             }
         }
     }
