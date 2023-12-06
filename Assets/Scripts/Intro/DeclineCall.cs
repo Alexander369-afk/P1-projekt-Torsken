e2cb5e95c3201989d7e9f7300a89af7e5f985b37
Taferz <52066169+Taferz@users.noreply.github.com>
2023-12-06 09:59:35 +0100

    Intro Change

--- Assets/Scripts/Intro/DeclineCall.cs
+++ Assets/Scripts/Intro/DeclineCall.cs
@@ -4,28 +4,40 @@ using DG.Tweening;
 using Unity.VisualScripting;
 using UnityEngine;
 
+
+
 public class DeclineCall : MonoBehaviour
-
 {
-
+        // Time before the GameObjects are reactivated
     [SerializeField] private float _callBackTime = 3f;
+        // Array of GameObjects that will be deactivated
     public GameObject[] declineButton;
-      void Start()
-    {
-        // declineButton = GameObject.FindGameObjectsWithTag("IntroTag");
-    }
+    
+        // Call this method to deactivate the GameObjects
     public void Call() {
         foreach (GameObject button in declineButton)
         {
-            button.SetActive(false);
+            FadeOut(button);
             
         }
+        // Reactivate the GameObjects after the specified time
         Invoke("ReactivateGameObjects",_callBackTime);
     }
+    // Call this method to reactivate the GameObjects
     void ReactivateGameObjects() {
-
         foreach (GameObject button in declineButton)
         {
-            button.SetActive(true);
+            FadeIn(button);
         }}
+    // Fade out the GameObjects
+    void FadeOut(GameObject obj) {
+        obj.GetComponent<CanvasGroup>().DOFade(0f, 1f);
+        obj.GetComponent<CanvasGroup>().interactable = false;
+    }
+
+    // Fade in the GameObjects
+    void FadeIn(GameObject obj) {
+        obj.GetComponent<CanvasGroup>().DOFade(1f, 1f);
+        obj.GetComponent<CanvasGroup>().interactable = true;
+    }
 }
