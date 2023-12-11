05fa7c882e7ce460e7fb0ea5a7bf57f9ebebd38e
natconrad <ncol23@student.aau.dk>
2023-12-11 16:21:25 +0100

    Game3

--- Assets/Scripts/Game_3/DragDropWheel.cs
+++ Assets/Scripts/Game_3/DragDropWheel.cs
@@ -8,12 +8,25 @@ public class DragDropWheel : MonoBehaviour
     public delegate void DragEndedDelegate(DragDropWheel wheelObject);
 
     public DragEndedDelegate dragEndedCallback;
-    
-    
+
+    public float delay = 2;         //https://gamedevbeginner.com/how-to-delay-a-function-in-unity/ delay on wheel rotate after snap
+    float timer;
+
+   
+
     bool dragged = false;           //variable either being true or false
+    bool trigged = false;
+
 
     Vector3 offset;
-    Vector3 wheelDragStartPosition;
+
+    //Vector3 wheelDragStartPosition;
+
+    //public GameObject Wheel;
+    public ParticleSystem Nutrition;
+    public ParticleSystem NutritionBack;
+
+    public Vector3 rotateAmount;
 
     private void OnMouseDown()      //Unity function; whenever mouse button is
                                     //clicked down on an object this function will get called. 
@@ -56,5 +69,41 @@ public class DragDropWheel : MonoBehaviour
             transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition) + offset;
 
         }
+
+        if (trigged)
+        {
+            timer += Time.deltaTime;
+
+            if (timer > delay)
+            {
+                transform.Rotate(rotateAmount * Time.deltaTime);
+            }
+
+        }
+
+       
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)         //https://www.youtube.com/watch?v=jwEPKY9poa4 bruger ikke alligvel
+    {
+        Debug.Log("We have collided");
+
+        trigged = true;
+    }
+
+  void OnTriggerStay2D(Collider2D collison)
+    {
+        Debug.Log("We are still colliding");
+
+        Destroy(Nutrition, 5f);
+        Destroy(NutritionBack, 5f);
+
+        Invoke("StopTrigged", 6.5f);      //ChatGpt helped
+
+    }
+
+    void StopTrigged()
+    {
+        trigged = false;
     }
 }
