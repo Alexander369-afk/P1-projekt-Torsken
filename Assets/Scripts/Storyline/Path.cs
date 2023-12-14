94d245a1de0030c9dd82d4ee19edb85937f1d765
Garioo <marius@qvarnstrom.dk>
2023-12-14 13:35:59 +0100

    Changes

--- Assets/Scripts/Storyline/Path.cs
+++ Assets/Scripts/Storyline/Path.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+using System;
 using UnityEngine;
 
 public class Path : MonoBehaviour
@@ -6,10 +6,13 @@ public class Path : MonoBehaviour
     [SerializeField] Transform[] Waypoints;
     [SerializeField] private float[] moveSpeeds; // Array of move speeds for each waypoint
     [SerializeField] private float[] waitTimes;  // Array of wait times for each waypoint
+    [SerializeField] private Collider[] audioColliders; // Array of colliders for each waypoint's audio
+    [SerializeField] private string[] audioClipNames; // Array of audio clip names for each waypoint
 
     private float timer;
     private int waypointsIndex;
     private AudioManager audioManager;
+    private bool audioPlayed;
 
     void Awake()
     {
@@ -26,6 +29,7 @@ public class Path : MonoBehaviour
         AudioManager.instance.Play("Havlyden Loop");
         waypointsIndex = 0;
         timer = 0f;
+        audioPlayed = false;
     }
 
     void Update()
@@ -38,21 +42,21 @@ public class Path : MonoBehaviour
             {
                 // Move towards the waypoint with the specified move speed
                 transform.position = Vector2.MoveTowards(transform.position, Waypoints[waypointsIndex].position, moveSpeeds[waypointsIndex] * Time.deltaTime);
+
+                // Reset audioPlayed flag while moving towards the waypoint
+                audioPlayed = false;
             }
             else
             {
                 // Arrived at the waypoint, play audio and start waiting
-                if (waypointsIndex == 0)
+                if (!audioPlayed)
                 {
-                    AudioManager.instance.Play("CutScene0");
-                }
-                else if (waypointsIndex == 1)
-                {
-                    AudioManager.instance.Play("BakterieSnak");
-                }
-                else if (waypointsIndex == 4)
-                {
-                    AudioManager.instance.Play("Spil1Forklaring");
+                    if (waypointsIndex < audioClipNames.Length)
+                    {
+                        AudioManager.instance.Play(audioClipNames[waypointsIndex]);
+                    }
+
+                    audioPlayed = true; // Set flag to true so audio is not played again
                 }
 
                 timer += Time.deltaTime;
@@ -67,10 +71,24 @@ public class Path : MonoBehaviour
         }
     }
 
+    // OnTriggerEnter is called when the Collider other enters the trigger
+    void OnTriggerEnter(Collider other)
+    {
+        // Check if the collider is in the audioColliders array
+        int colliderIndex = Array.IndexOf(audioColliders, other);
+
+        if (colliderIndex != -1 && colliderIndex < audioClipNames.Length)
+        {
+            // Player collided with the collider associated with the current waypoint, trigger audio
+            AudioManager.instance.Play(audioClipNames[colliderIndex]);
+        }
+    }
+
     public void WheelSnapped()
     {
         // Implement the logic to handle waypoint change when the wheel is snapped
         // For example, you can increment waypointsIndex or set a specific waypoint index
         waypointsIndex = (waypointsIndex + 1) % Waypoints.Length;
+        audioPlayed = false; // Reset the audioPlayed flag when changing waypoints
     }
 }
