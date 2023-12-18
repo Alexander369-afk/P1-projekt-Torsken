ce8abf9dfec496aeda342b5f9dba6bb0f9795d50
Garioo <marius@qvarnstrom.dk>
2023-12-18 19:29:03 +0100

    heyyy

--- Assets/Scripts/ColliderAudio.cs
+++ Assets/Scripts/ColliderAudio.cs
@@ -4,14 +4,53 @@ using UnityEngine;
 
 public class ColliderAudio : MonoBehaviour
 {
-   public AudioManager audioManager;
+    public AudioManager audioManager;
 
     public void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.CompareTag("AudioCutscene"))
+        //Start Statements
+        if (other.CompareTag("AudioCutscene"))
         {
             AudioManager.instance.Play("Cutscene 2");
             Debug.Log("Afspiller Lyd til cutscene 2");
+            // Deactivate the collider on the other GameObject
+            other.enabled = false;
+        }
+
+        if (other.CompareTag("IntroMusic"))
+        {
+            AudioManager.instance.Play("IntroMusic");
+            Debug.Log("Afspiller Lyd til IntroMusic");
+            other.enabled = false;
+        }
+
+        if (other.CompareTag("Spil 1 Forklaring"))
+        {
+            AudioManager.instance.Play("Spil 1 Forklaring");
+            Debug.Log("Afspiller Lyd til Spil 1 Forklaring");
+            other.enabled = false;
+        }
+
+        if (other.CompareTag("Spil 1 Music"))
+        {
+            AudioManager.instance.Play("Spil 1 Music");
+            Debug.Log("Afspiller Lyd til Spil 1 Music");           
+            other.enabled = false;
+        }
+
+        if (other.CompareTag("Cutscene 2 Music"))
+        {
+            AudioManager.instance.Play("Cutscene 2 Music");
+            Debug.Log("Afspiller Lyd til Cutscene 2 Music");
+            other.enabled = false;
+        }
+
+        //Stop Statements
+        if (other.CompareTag("Spil 1 Music Stop"))
+        {
+            AudioManager.instance.Stop("Spil 1 Music");
+            Debug.Log("Afspiller Lyd til Spil 1 Music");
+            other.enabled = false;
         }
     }
 }
--- Assets/Scripts/Storyline/Path.cs
+++ Assets/Scripts/Storyline/Path.cs
@@ -1,30 +1,20 @@
 using System;
 using UnityEngine;
 
-[Serializable]
-public class WaypointAudio
-{
-    public Collider2D collider;
-    public string audioClipName;
-}
-
 public class Path : MonoBehaviour
 {
-    [SerializeField] Transform[] Waypoints;
-    [SerializeField] private float[] moveSpeeds; // Array of move speeds for each waypoint
-    [SerializeField] private float[] waitTimes;  // Array of wait times for each waypoint
-    [SerializeField] private WaypointAudio[] waypointAudios; // Array of collider and audio clip pairs
+    [SerializeField] Transform[] Waypoints; // Array that sets a GameObject as a waypoint
+    [SerializeField] private float[] moveSpeeds; // Array that sets the move speed from one waypoint to the next
+    [SerializeField] private float[] waitTimes;  // Array for amount of seconds Torsken should wait at each waypoint
 
     private float timer;
     private int waypointsIndex;
-    private bool audioPlayed;
 
     void Start()
     {
         AudioManager.instance.Play("Havlyden Loop");
         waypointsIndex = 0;
         timer = 0f;
-        audioPlayed = false;
     }
 
     void Update()
@@ -36,49 +26,15 @@ public class Path : MonoBehaviour
             if (distance > 0.1f)
             {
                 // Move towards the waypoint with the specified move speed
-                transform.position = Vector2.MoveTowards(transform.position, Waypoints[waypointsIndex].position, moveSpeeds[waypointsIndex] * Time.deltaTime);
-
-                // Reset audioPlayed flag while moving towards the waypoint
-                audioPlayed = false;
+                transform.position = Vector2.MoveTowards(transform.position,
+                    Waypoints[waypointsIndex].position, moveSpeeds[waypointsIndex] * Time.deltaTime);
             }
-            else
+            if (timer >= waitTimes[waypointsIndex])
             {
-                // Arrived at the waypoint, play audio and start waiting
-                if (!audioPlayed)
-                {
-                    if (waypointsIndex < waypointAudios.Length)
-                    {
-                        AudioManager.instance.Play(waypointAudios[waypointsIndex].audioClipName);
-                    }
-
-                    audioPlayed = true; // Set flag to true so audio is not played again
-                }
-
-                timer += Time.deltaTime;
-
-                if (timer >= waitTimes[waypointsIndex])
-                {
                     // Reset the timer and move to the next waypoint
                     timer = 0f;
                     waypointsIndex++;
-                }
             }
-        }
-    }
-
-    // OnTriggerEnter2D is called when the Collider2D other enters the trigger
-    void OnTriggerEnter2D(Collider2D other)
-    {
-        // Check if the collider is in the waypointAudios array
-        int colliderIndex = Array.FindIndex(waypointAudios, wa => wa.collider == other);
-
-        if (colliderIndex != -1 && colliderIndex < waypointAudios.Length)
-        {
-            // Player collided with the collider associated with the current waypoint, trigger audio
-            AudioManager.instance.Play(waypointAudios[colliderIndex].audioClipName);
-
-            // Disable the collider
-            other.enabled = false;
         }
     }
 
@@ -87,7 +43,6 @@ public class Path : MonoBehaviour
         // Implement the logic to handle waypoint change when the wheel is snapped
         // For example, you can increment waypointsIndex or set a specific waypoint index
         waypointsIndex = (waypointsIndex + 1) % Waypoints.Length;
-        audioPlayed = false; // Reset the audioPlayed flag when changing waypoints
     }
 }
 
